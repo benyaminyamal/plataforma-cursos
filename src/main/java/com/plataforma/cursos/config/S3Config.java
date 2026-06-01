edc7bee73076a5fe499efaf3eb49dc4fd1d10749
benclaros <claros.benjamin@icloud.com>
2026-06-01 13:55:04 -0400

    fix: usar OpenPDF 1.3.43 (compatible con Java 17) + apache-client para AWS

--- src/main/java/com/plataforma/cursos/config/S3Config.java
+++ src/main/java/com/plataforma/cursos/config/S3Config.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.http.apache.ApacheHttpClient;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
@@ -22,6 +23,7 @@ public class S3Config {
         return S3Client.builder()
                 .region(Region.of(region))
                 .credentialsProvider(DefaultCredentialsProvider.create())
+                .httpClientBuilder(ApacheHttpClient.builder())
                 .build();
     }
 }
